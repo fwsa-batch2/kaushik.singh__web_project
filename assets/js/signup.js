4382034d2358d64fb00eca7d1ddf2de1421caebd
kaushik-08 <ckaushicksingh2003@gmail.com>
2021-12-10 11:09:50 +0530

    update project files

---
+++ assets/js/signup.js
@@ -0,0 +1,68 @@
+
+
+  let signUp = [];
+
+  function pageonload(){
+    const user = JSON.parse(localStorage.getItem("Users_Details"));
+    if(user != null){
+      signUp=user;
+    }
+  }
+  
+  pageonload()
+  
+function check_password() {
+  event.preventDefault();
+
+  let name = document.getElementById("username").value;
+  let secondname = document.getElementById("secondname").value;
+  let gmail = document.getElementById("email").value;
+  let dob = document.getElementById("date").value;
+  let Create = document.getElementById("password").value;
+  let Confirm = document.getElementById("confirm_password").value;
+
+  let isEmailExist = emailExist(gmail);
+
+  if (Create != Confirm) {
+    alert("Incorrect password");   
+  }
+  else if(isEmailExist){
+    alert("Email Already Exist");  
+  }
+  else {
+    const customer_details = {
+      "Firstname": name,
+      "second name": secondname,
+      "email": gmail,
+      "DOB": dob,
+      "password": Create,
+      "confrim_pass": Confirm
+      }
+  
+    signUp.push(customer_details);
+    localStorage.setItem("Users_Details", JSON.stringify(signUp));
+    window.location.href = "./bike.html";
+  }
+
+}
+
+// function add(num1, num2){
+//     Total = num1+ num2;
+//     alert(Total);
+// }
+
+// add(140000000, 23);
+
+function emailExist(currentEmail){
+  let used = false;
+    for (i = 0; i < signUp.length; i++) {
+        const email = signUp[i].email;
+        if (currentEmail == email) {
+            used = true;
+            break;
+        }
+    }
+    return used;
+
+}
+
