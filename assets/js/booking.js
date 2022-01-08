ceb0aba329c12cbe387d1fc8834fe439975759d6
kaushik-08 <ckaushicksingh2003@gmail.com>
2022-01-08 16:30:32 +0530

    booking js

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -5,12 +5,32 @@ function onPageLoad() {
 }
 onPageLoad();
 
+function pageonload(){
+    const user = JSON.parse(localStorage.getItem("booked_Details"));
+    if(user != null){
+      bookList=user;
+    }
+  }
+  
+  pageonload()
+
 function booked(){
     event.preventDefault();
     const name = document.getElementById("User_name").value; 
     const email = document.getElementById("email").value; 
     const dateOfBooking = document.getElementById("Date_booking").value; 
     const address = document.getElementById('Address').value;
+
+    const customer_details = {
+        "Firstname": name,
+        "email": email,
+        "DOB": dateOfBooking,
+        "delevery_Address": address,
+        }
+
+    bookList.push(customer_details);
+    localStorage.setItem("booked_Details", JSON.stringify(bookList));
+    window.location.href = "../../pages/ended.html";
 }
 
 
