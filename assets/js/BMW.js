048655749564de5367a88ff6790599cda75ff1fe
kaushik-08 <ckaushicksingh2003@gmail.com>
2022-01-12 12:45:04 +0530

    code smell

--- assets/js/BMW.js
+++ assets/js/BMW.js
@@ -4,32 +4,33 @@ function big(img) {
 
     switch (img) {
         case "../assets/images/s1000rr.png":
-            document.getElementById('engn').src = "../../assets/images/G310-gaurd.jpg";
-            document.getElementById('hand').src = "../../assets/images/G310-silincer.jpeg";
-            document.getElementById('spedo').src = "../../assets/images/g";
-            document.getElementById('suspen').src = "../assets/images/suspension-1250.png";
+            document.getElementById('engn').src = "../../assets/images/s1000rr_engine.png";
+            document.getElementById('hand').src = "../../assets/images/s1200r-spedometer.jpg";
+            document.getElementById('spedo').src = "../../assets/images/s100rr_suspension.png";
+            document.getElementById('suspen').src = "../../assets/images/s100rr_silencer.png";
             break;
 
         case "../assets/images/bmw-G310.png":
             document.getElementById('engn').src = "../../assets/images/G310-gaurd.jpg";
-            document.getElementById('hand').src = "../../assets/images/G310-silincer.jpeg";
-            document.getElementById('spedo').src = "../../assets/images/g";
-            document.getElementById('suspen').src = "../assets/images/suspension-1250.png";
+            document.getElementById('hand').src = "../../assets/images/G310_speodometer2.png";
+            document.getElementById('spedo').src = "../../assets/images/G310_suspension.png";
+            document.getElementById('suspen').src = "../../assets/images/G310-silincer.jpeg";
             break;
 
         case "../assets/images/BMW-1250-rpng":
-            document.getElementById('engn').src = "../../assets/images/handelbar-1250.jpeg";
+            document.getElementById('engn').src = "../../assets/images/1250-engine.png";
             document.getElementById('hand').src = "../../assets/images/spedo-1250.png";
             document.getElementById('spedo').src = "../../assets/images/suspension-1250.png";
-            document.getElementById('suspen').src = "../../assets/images/spedo-1250.png";
+            document.getElementById('suspen').src = "../../assets/images/handelbar-1250.jpeg";
             break;
 
         case "../../assets/images/bmw-r1200gs.png":
             document.getElementById('engn').src = "../../assets/images/1200r engine.jpg";
             document.getElementById('hand').src = "../../assets/images/1200r-handlebar.jpeg";
             document.getElementById('spedo').src = "../../assets/images/1200r-suspension.png";
-            document.getElementById('suspen').src = "../../assets/images/1250-engine.png";
+            document.getElementById('suspen').src = "../../assets/images/1200rgs_silencer.png";
             break;
+
 
     }
 }
