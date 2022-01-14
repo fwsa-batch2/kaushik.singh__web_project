d93eba7c228de0f0e5203d824464a994e1069982
kaushik-08 <ckaushicksingh2003@gmail.com>
2022-01-15 00:35:31 +0530

    aaligment

---
+++ assets/js/dugati.js
@@ -0,0 +1,11 @@
+function big(img) {
+    document.querySelector('.big').src = img;
+}
+function path(color) {
+    const path = document.querySelector('.path');
+    path.style.background = color;
+}
+function bg(background) {
+    const bg = document.querySelector('.cont');
+    bg.style.background = background;
+}
