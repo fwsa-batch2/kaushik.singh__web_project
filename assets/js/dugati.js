82076528151ce3c4e759c936da359d94f329c3bb
kaushik-08 <ckaushicksingh2003@gmail.com>
2022-01-15 00:42:37 +0530

    code smell

--- assets/js/dugati.js
+++ assets/js/dugati.js
@@ -2,10 +2,10 @@ function big(img) {
     document.querySelector('.big').src = img;
 }
 function path(color) {
-    const path = document.querySelector('.path');
-    path.style.background = color;
+    const guga= document.querySelector('.path');
+    duga.style.background = color;
 }
 function bg(background) {
-    const bg = document.querySelector('.cont');
-    bg.style.background = background;
+    const concolr = document.querySelector('.cont');
+    concolr.style.background = background;
 }
